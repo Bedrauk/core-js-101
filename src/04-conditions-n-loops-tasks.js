340d4bf7b89e4dd0786198e9b53eba22f5ee8a9e
Bedrauk <111381113+Bedrauk@users.noreply.github.com>
2024-09-10 18:49:36 +0300

    docs: edit 4 task

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -506,17 +506,17 @@ function getMatrixProduct(m1, m2) {
 function evaluateTicTacToePosition(position) {
   for (let i = 0; i < 3; i++) {
     if (position[i][0] !== undefined && position[i][0] === position[i][1] && position[i][1] === position[i][2]) {
-        return position[i][0];
+      return position[i][0];
     }
     if (position[0][i] !== undefined && position[0][i] === position[1][i] && position[1][i] === position[2][i]) {
-        return position[0][i];
+      return position[0][i];
     }
   }
   if (position[0][0] !== undefined && position[0][0] === position[1][1] && position[1][1] === position[2][2]) {
-      return position[0][0];
+    return position[0][0];
   }
   if (position[0][2] !== undefined && position[0][2] === position[1][1] && position[1][1] === position[2][0]) {
-      return position[0][2];
+    return position[0][2];
   }
   return undefined;
 }
@@ -541,5 +541,3 @@ module.exports = {
   getMatrixProduct,
   evaluateTicTacToePosition,
 };
-
-console.log(evaluateTicTacToePosition([[ 'X',   ,'0' ], [    ,'X','0' ], [    ,   ,'0' ]]))
